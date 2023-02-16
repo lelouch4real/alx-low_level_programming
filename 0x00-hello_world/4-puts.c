2ffda92a985fcfdd285e6983e3ca9a176e17d536
Abdelbasset <y.abdelbasset.achou@gmail.com>
2023-02-16 15:25:56 -0800

    lol

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a c program that prints a line using outs function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
