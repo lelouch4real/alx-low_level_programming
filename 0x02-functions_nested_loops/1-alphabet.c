25437b553c2d95ea547c11f15e9a720fa566b30c
Abdelbasset <y.abdelbasset.achou@gmail.com>
2023-02-21 17:27:55 -0800

    lol

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- *
- * main - entr
- *
+ * print_alphabet - Entry point
+ * Description: Prints alphabets
  * Return: always return 0
- *
  */
+
 void print_alphabet(void)
 {
 	char i;
